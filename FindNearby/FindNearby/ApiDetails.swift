b5cf2b7e38c3c3ef4c15d04441f54255d64998bf
Sajid Shanta <Sajid@Mohammads-Mac-mini.local>
2023-05-28 15:42:56 +0600

    implement find nearby hospital 1

---
+++ FindNearby/FindNearby/ApiDetails.swift
@@ -0,0 +1,12 @@
+//
+//  ApiDetails.swift
+//  FindNearby
+//
+//  Created by Sajid Shanta on 28/5/23.
+//
+
+import UIKit
+
+class ApiDetails: NSObject {
+
+}
---
+++ FindNearby/FindNearby/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  FindNearby
+//
+//  Created by Sajid Shanta on 28/5/23.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+    var nameArray = [String]()
+    let locationManager = CLLocationManager()
+    
+    var lat: Double = 0.00
+    var long: Double = 0.00
+    
+    let myAPIKey = "AIzaSyBiyh17wRHqgPgbdWW7tENfnFBwjMeT6ko"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        searchApiPlaceFromGoogle()
+    }
+    
+    @IBOutlet var txtView: UITextView!
+    @IBAction func btnHospital(_ sender: Any) {
+        self.printPlaceNames()
+    }
+    
+    func searchApiPlaceFromGoogle() {
+        // get permissions of location services
+        locationManager.requestWhenInUseAuthorization()
+        
+        // get current location
+//        var currentLocation: CLLocation!
+//        currentLocation = locationManager.location
+//
+//        self.lat = currentLocation.coordinate.latitude
+//        self.long = currentLocation.coordinate.longitude
+        
+        // print location on console
+        print(self.lat)
+        print(self.long)
+        
+        // using Google API url: Please register in Google nearby place API key to get the API key
+        let stringGoogleApi = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=clinic&type=clinic&key=\(myAPIKey)"
+        let url = NSURL(string: stringGoogleApi)
+        URLSession.shared.dataTask(with: (url as URL?)!, completionHandler: { (data, response, error) -> Void in
+            if let jsonObject = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
+                if let placeArray = jsonObject.value(forKey: "results") as? NSArray {
+                    print("placeArray \(placeArray)")
+                    for place in placeArray {
+                        print("place \(place)")
+                        if let placeDict = place as? NSDictionary {
+                            print("placeDict \(placeDict)")
+                            if let name = placeDict.value(forKey: "name") {
+                                print("name \(name)")
+                                self.nameArray.append(name as! String)
+                            }
+                        }
+                    }
+                }
+            }
+        }).resume()
+    }
+    func printPlaceNames() {
+        print("kk")
+        print(nameArray.joined(separator: "\n \n") + "nn")
+        self.txtView.text = nameArray.joined(separator: "\n \n")
+    }
+}
+
