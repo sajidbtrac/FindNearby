d0f98a9fd085ee2a657ef6a8d464a69ea9fb82fc
sajid <sajidhasan673@gmail.com>
2023-05-29 16:52:29 +0600

    show nearby places on map

--- FindNearby/FindNearby/ViewController.swift
+++ FindNearby/FindNearby/ViewController.swift
@@ -6,13 +6,16 @@
 //
 
 import UIKit
+import MapKit
 import CoreLocation
 
 class ViewController: UIViewController {
     let queryTypes = ["hospital", "police", "atm"]
     var queryType: String = "hospital"
     
-    var nameArray = [String]()
+    let maxDistance: Double = 2000
+    
+    //    var nameArray = [String]()
     let locationManager = CLLocationManager()
     
     var lat: Double = 0.00
@@ -27,16 +30,22 @@ class ViewController: UIViewController {
         typePicker.delegate = self
         
         searchApiPlaceFromGoogle()
+        
     }
     
     @IBOutlet var typePicker: UIPickerView!
-    @IBOutlet var txtView: UITextView!
-    @IBAction func btnHospital(_ sender: Any) {
-        self.printPlaceNames()
-    }
+    //    @IBOutlet var txtView: UITextView!
+    @IBOutlet weak var mainMapView: MKMapView!
+    //    @IBAction func btnHospital(_ sender: Any) {
+    //        self.printPlaceNames()
+    //    }
     
     func searchApiPlaceFromGoogle() {
-        self.nameArray.removeAll()
+        //        self.nameArray.removeAll()
+        
+        // reset map
+        let allAnnotations = self.mainMapView.annotations
+        self.mainMapView.removeAnnotations(allAnnotations)
         
         // get permissions of location services
         locationManager.requestWhenInUseAuthorization()
@@ -44,9 +53,14 @@ class ViewController: UIViewController {
         // get current location
         var currentLocation: CLLocation!
         currentLocation = locationManager.location
-
+        
+        // set map region around users current location
+        mainMapView.setRegion(MKCoordinateRegion(center: currentLocation.coordinate, latitudinalMeters: maxDistance, longitudinalMeters: maxDistance), animated: true)
+        mainMapView.tintColor = .blue
+        
         self.lat = currentLocation.coordinate.latitude
         self.long = currentLocation.coordinate.longitude
+        
         // print location on console
         print(self.lat)
         print(self.long)
@@ -61,8 +75,20 @@ class ViewController: UIViewController {
                 if let placeArray = jsonObject.value(forKey: "results") as? NSArray {
                     for place in placeArray {
                         if let placeDict = place as? NSDictionary {
-                            if let name = placeDict.value(forKey: "name") {
-                                self.nameArray.append(name as! String)
+                            if let name = placeDict.value(forKey: "name"),
+                               let geometry = placeDict.value(forKey: "geometry"),
+                               let loc = (geometry as AnyObject).value(forKey: "location"),
+                               let lat = (loc as AnyObject).value(forKey: "lat"),
+                               let lng = (loc as AnyObject).value(forKey: "lng") {
+                                
+                                //self.nameArray.append(name as! String)
+                                let annotation = MKPointAnnotation()
+                                annotation.title = name as? String ?? ""
+                                annotation.coordinate = CLLocationCoordinate2D(latitude: lat as! CLLocationDegrees, longitude: lng as! CLLocationDegrees)
+                                
+                                print()
+                                // add the location annotation to map
+                                self.mainMapView.addAnnotation(annotation)
                             }
                         }
                     }
@@ -70,20 +96,20 @@ class ViewController: UIViewController {
             }
         }).resume()
     }
-    func printPlaceNames() {
-        self.txtView.text = nameArray.joined(separator: "\n \n")
-    }
+    //    func printPlaceNames() {
+    //        self.txtView.text = nameArray.joined(separator: "\n \n")
+    //    }
 }
 
 extension ViewController: UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-
+    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return queryTypes.count
     }
-
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         print(queryTypes[row])
         self.queryType = self.queryTypes[row]
